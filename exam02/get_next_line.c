e9830a02815d29ae070d1b35839014120d936408
tjdwns5063@naver.com <tjdwns5063@naver.com>
2021-12-01 13:09:28 +0900

    update get_next_line

--- exam02/get_next_line.c
+++ exam02/get_next_line.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#define BUFFER_SIZE 3
+#define BUFFER_SIZE 4
 
 int	ft_strlen(char *str)
 {
@@ -67,29 +67,92 @@ char	*ft_strjoin(char *str, char *buf)
 	return (ret);
 }
 
+int	str_in_nl(char *str)
+{
+	int	idx;
+
+	idx = 0;
+	while (str[idx])
+	{
+		if (str[idx] == '\n')
+			return (idx);
+		idx++;
+	}
+	return (-1);
+}
+
+char	*make_line(char **str)
+{
+	char	*ret;
+	char	*tmp;
+	int		s_idx;
+	int		idx;
+
+	s_idx = str_in_nl(*str);
+	ret = (char *)malloc(sizeof(char) * (s_idx + 2));
+	idx = 0;
+	while (idx <= s_idx)
+	{
+		ret[idx] = (*str)[idx];
+		idx++;
+	}
+	ret[idx] = 0;
+	tmp = ft_strdup(*str + (s_idx + 1));
+	free(*str);
+	*str = tmp;
+	return (ret);	
+}
+
 char	*get_next_line(int fd)
 {
-	static char	*str = 0;
+	static char	*str;
 	char		*buf;
 	char		*line;
+	int			size;
 
-	if (fd < 0)
+	if (fd < 0 | BUFFER_SIZE <= 0)
 		return (0);
 	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	buf[BUFFER_SIZE] = 0;
-	while (read(fd, buf, BUFFER_SIZE) > 0)
+	if (!buf)
+		return (0);
+	size = read(fd, buf, BUFFER_SIZE);
+	if (size <= 0)
 	{
-		printf("buf : %s\n", buf);
-		str = ft_strjoin(str, buf);
-		printf("str : %s\n", str);
+		free(buf);
+		return (0);
 	}
+	while (size > 0)
+	{
+		buf[size] = 0;
+		str = ft_strjoin(str, buf);
+		if (str_in_nl(str) >= 0)
+			break ;
+		size = read(fd, buf, BUFFER_SIZE);
+	}
+	free(buf);
+	if (size == 0)
+	{
+		free(line);
+		line = 0;
+		line = str;
+		str = 0;
+		return (line);
+	}
+	line = make_line(&str);
+	return (line);
 }
 
 int	main(void)
 {
 	int fd;
+	char *line;
 
-	fd = open("test", O_RDONLY);
-	while (get_next_line(fd));
-	return (0);
+	fd = open("test2", O_RDONLY);
+	while ((line = get_next_line(fd)))
+	{
+		printf("line : %s\n", line);
+		free(line);
+	}
+//	printf("line : %s\n", line);
+	system("leaks a.out | grep leaked");
 }
